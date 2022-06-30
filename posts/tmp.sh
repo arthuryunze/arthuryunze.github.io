41a57adca4584cd7709639f1d546f2d2732aa119
arthuryunze <arthuryunze@users.noreply.github.com>
2022-06-30 01:17:54 +0000

    deploy: baa7805bb67b878305d60bfe65fd146be68a0e58

--- posts/tmp.sh
+++ posts/tmp.sh
@@ -3,11 +3,12 @@ do
     if test -f $file
     then
         # echo $file 是文件
-        echo ${file:2:10}
-        # name=${file:13:-3}
-        time=${file:2:10}
+        echo ${file:13}
+        name=${file:13}
+        # time=${file:2:11}
         # sed -i "s//${name}/g" $file
-        sed -i "s/tmp.sh/${time}/g" ${file}
+        # sed -i "s/tmp.sh/${time}/g" ${file}
+        mv $file $name
     fi
     if test -d $file
     then
